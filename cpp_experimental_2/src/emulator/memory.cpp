e0287b4d2bec26531dc7bd6586233c8c845f4f73
SaikrishnaTadepalli <saikrishna.t328@gmail.com>
2025-01-04 04:16:43 -0500

    Wrote code for directory setup + main-memory. Need to test main-memory functions.

---
+++ cpp_experimental_2/src/emulator/memory.cpp
@@ -0,0 +1,100 @@
+#include "memory.hpp"
+
+Memory::Memory(std::vector<uint32_t> code) {
+    /*
+        - code is a list of words
+        - if the last word of the code fit's into memory, access is valid
+        - byte_index of the last word of the code = ((code.size() - 1) * 4)
+    */
+    this->validate_access(
+        ((code.size() - 1) * (int)AccessLength::WORD), 
+        AccessLength::WORD
+    );
+
+    // Copy the code into main memory
+    std::memcpy(this->memory, code.data(), (code.size() * sizeof(uint32_t)));
+}
+
+void Memory::validate_access(uint byte_index, AccessLength length) {
+    if ((byte_index < 0) || (byte_index + (int)length > MEM_SIZE)) {
+        // Throw Exception
+        perror("Error in Memory Access.");
+    }
+}
+
+uint8_t Memory::get_byte(uint byte_index) {
+    this->validate_access(byte_index, AccessLength::BYTE);
+    
+    uint8_t data;
+    // Reading data from main-memory
+    std::memcpy(&data, (this->memory + byte_index), sizeof(uint8_t));
+
+    return data;
+}
+
+void Memory::set_byte(uint byte_index, uint8_t val) {
+    this->validate_access(byte_index, AccessLength::BYTE);
+
+    // Copying val to main-memory
+    std::memcpy((this->memory + byte_index), &val, sizeof(uint8_t));
+}
+
+uint32_t Memory::get_word(uint byte_index) {
+    this->validate_access(byte_index, AccessLength::WORD);
+    
+    uint32_t data;
+    // Reading data from main-memory
+    std::memcpy(&data, (this->memory + byte_index), sizeof(uint32_t));
+    
+    return data;
+}
+
+void Memory::set_word(uint byte_index, uint32_t val) {
+    this->validate_access(byte_index, AccessLength::WORD);
+
+    // Copying val to main-memory
+    std::memcpy((this->memory + byte_index), &val, sizeof(uint32_t));
+}
+
+uint64_t Memory::get_double_word(uint byte_index) {
+    this->validate_access(byte_index, AccessLength::DOUBLE_WORD);
+
+    uint64_t data;
+    // Reading data from main-memory
+    std::memcpy(&data, (this->memory + byte_index), sizeof(uint64_t));
+    
+    return data;
+}
+
+void Memory::set_double_word(uint byte_index, uint64_t val) {
+    this->validate_access(byte_index, AccessLength::DOUBLE_WORD);
+    
+    // Copying val to main-memory
+    std::memcpy((this->memory + byte_index),&val, sizeof(uint64_t));
+}
+
+void Memory::randomize() {
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+
+    for (int mem_addr = 0; mem_addr < MEM_SIZE; mem_addr+=8) {
+        std::uniform_int_distribution<int64_t> dist;
+        int64_t val = dist(gen);
+        this->set_double_word(mem_addr, val);
+    }
+
+
+    // THERE SHOULD BE A SYSTEM CALL FOR THIS.
+    // '/dev/urandom' generates random bytes
+    // We should be able to use this to generate X bytes of random data
+    //  and read it directly into the emulator's main-memory
+    // FIGURE OUT AN APPROACH SIMILAR TO THE ONE BELOW 
+    //  (not compiling because 'getrandom' not found on MacOS)
+
+
+    // Use 'getrandom' call to fill the array with random values
+    // ssize_t result = getrandom(this->memory, MEM_SIZE, 0);
+    // if (result == -1) {
+    //     perror("Error in 'getrandom' call.");  // Handle error
+    // }
+}
---
+++ cpp_experimental_2/src/emulator/memory.hpp
@@ -0,0 +1,34 @@
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <sys/random.h>
+#include <unistd.h>
+
+// in Bytes
+#define MEM_SIZE 1024
+
+class Memory {
+public:
+    uint8_t memory[MEM_SIZE] = { 0 };
+    enum AccessLength { BYTE = 1, WORD = 4, DOUBLE_WORD = 8};
+
+    Memory(std::vector<uint32_t> code);
+
+    void validate_access(uint byte_index, AccessLength length);
+
+    uint8_t get_byte(uint byte_index);
+    void set_byte(uint byte_index, uint8_t val);
+
+    uint32_t get_word(uint word_index);
+    void set_word(uint word_index, uint32_t val);
+
+    uint64_t get_double_word(uint double_word_index);
+    void set_double_word(uint double_word_index, uint64_t val);
+
+    void randomize();
+};
+
+#endif
